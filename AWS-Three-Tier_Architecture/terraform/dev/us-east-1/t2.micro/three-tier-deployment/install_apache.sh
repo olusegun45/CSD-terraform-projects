789eab806094ace508290918ae445d225750fd86
pogunyemi45 <pogunyemi45@gmail.com>
2023-01-27 19:00:09 -0600

    rdme

--- AWS-Three-Tier_Architecture/terraform/dev/us-east-1/t2.micro/three-tier-deployment/install_apache.sh
+++ AWS-Three-Tier_Architecture/terraform/dev/us-east-1/t2.micro/three-tier-deployment/install_apache.sh
@@ -5,4 +5,4 @@ systemctl start httpd
 systemctl enable httpd
 echo "Hello World from $(hostname -f)" > /var/www/html/index.html
 
-sudo echo "<html><body><img src='https://github.com/olusegun45/4tiers-Network-terra/blob/main/AWS%20Net%20diagram%2010-17-21.jpeg?raw=true' alt='My Image'/></body></html>" > /var/www/html/index.html
+sudo echo "<html><body><img src='https://github.com/olusegun45/CSD-terraform-projects/blob/main/AWS%20Net%20diagram%2010-17-21%20(2).jpeg?raw=true' alt='My Image'/></body></html>" > /var/www/html/index.html
